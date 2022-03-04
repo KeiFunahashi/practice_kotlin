87068125d407dc02910a2d93e2312b283fb444e6
船橋渓 <funahashikei@team-lab.com>
2022-03-04 15:57:46 +0900

    first commit

---
+++ src/main/kotlin/com/funahashikei/practice_kotlin/PracticeKotlinApplication.kt
@@ -0,0 +1,10 @@
+package com.funahashikei.practice_kotlin
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticeKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<PracticeKotlinApplication>(*args)
+}
